d0c6e41035713ec9958045db2fa8cb3319709b3c
bonino97 <juanbonino97@gmail.com>
2020-05-31 20:23:19 -0300

    Finish tests

--- TP 2.1/Reestructuracion/index.py
+++ TP 2.1/Reestructuracion/index.py
@@ -37,8 +37,11 @@ class Index:
 
             #Python Random Generator
             arrayRandom = self.GeneradorRandom(index, seed)            
+            
             RandomParidadResult, RandomParidadFrec = self.Paridad(arrayRandom)
             RandomKsResult, RandomKsD, RandomKsDEstimada = self.KolmogorovSmirnov(arrayRandom)
+            RandomChiCuadradoResult, RandomChiCuadradoSuma, RandomChiCuadradoValorTabla, RandomChiCuadradoFrecEstimada = self.ChiCuadrado(arrayRandom)
+            RandomCorridaArribaAbajoResult, RandomCorridaArribaAbajoZ, RandomCorridaArribaAbajoMedia = self.CorridaArribaAbajo(arrayRandom)
 
             self.RandomWindow = Toplevel()             
 
@@ -47,46 +50,65 @@ class Index:
             ##############################################
 
             Label(self.RandomWindow, text = 'Generador Random', font="Helvetica 14 bold", fg = "green", anchor = CENTER).grid(row = 0, column = 0)
-
             Label(self.RandomWindow, text = '___________________________________________________', fg='black').grid(row = 1, column = 0)
-
-            Label(self.RandomWindow, text = 'Test de Paridad.', font="Helvetica 10 bold").grid(row = 2, column = 0, sticky = W)
-
+            Label(self.RandomWindow, text = 'Test de Paridad.', font="Helvetica 11 bold").grid(row = 2, column = 0, sticky = W)
             Label(self.RandomWindow, text = 'Resultado: ').grid(row = 3, column = 0, sticky = W)
-            
             if RandomParidadResult: 
                 Label(self.RandomWindow, text = str(RandomParidadResult), fg = 'green').grid(row = 3, column = 1, sticky = W)    
             else: 
                 Label(self.RandomWindow, text = str(RandomParidadResult), fg = 'red').grid(row = 3, column = 1, sticky = W)
-
             Label(self.RandomWindow, text = 'Frecuencia: ').grid(row = 4, column = 0, sticky = W)
             Label(self.RandomWindow, text = RandomParidadFrec).grid(row = 4, column = 1, sticky = W)
-
             Label(self.RandomWindow, text = '___________________________________________________', fg='black').grid(row = 5, column = 0)
 
             ###############################################
             # --- Generador Random Kolmogorov Smirnov --- #
             ###############################################
 
-            Label(self.RandomWindow, text = 'Test de Kolmogorov-Smirnov.', font="Helvetica 10 bold").grid(row = 6, column = 0, sticky = W)
-
-
+            Label(self.RandomWindow, text = 'Test de Kolmogorov-Smirnov.', font="Helvetica 11 bold").grid(row = 6, column = 0, sticky = W)
             Label(self.RandomWindow, text = 'Resultado: ').grid(row = 8, column = 0, sticky = W)
-            Label(self.RandomWindow, text = str(RandomKsResult)).grid(row = 8, column = 1, sticky = W)
-
+            if RandomKsResult:
+                Label(self.RandomWindow, text = str(RandomKsResult), fg = 'green').grid(row = 8, column = 1, sticky = W)
+            else:
+                Label(self.RandomWindow, text = str(RandomKsResult), fg = 'red').grid(row = 8, column = 1, sticky = W)
             Label(self.RandomWindow, text = 'D - Resultante: ').grid(row = 9, column = 0, sticky = W)
             Label(self.RandomWindow, text = RandomKsD).grid(row = 9, column = 1, sticky = W)
-
             Label(self.RandomWindow, text = 'D - Estimada: ').grid(row = 10, column = 0, sticky = W)
             Label(self.RandomWindow, text = RandomKsDEstimada).grid(row = 10, column = 1, sticky = W)
-
             Label(self.RandomWindow, text = '___________________________________________________', fg='black').grid(row = 11, column = 0)
 
             ###############################################
             # ---    Generador Random ChiCuadrado     --- #
             ###############################################
 
-            self.ChiCuadrado(arrayRandom)
+            Label(self.RandomWindow, text = 'Test de ChiCuadrado.', font="Helvetica 11 bold").grid(row = 12, column = 0, sticky = W)
+            Label(self.RandomWindow, text = 'Resultado: ').grid(row = 13, column = 0, sticky = W)
+            if RandomChiCuadradoResult:
+                Label(self.RandomWindow, text = str(RandomChiCuadradoResult), fg = 'green').grid(row = 13, column = 1, sticky = W)
+            else:
+                Label(self.RandomWindow, text = str(RandomChiCuadradoResult), fg = 'red').grid(row = 13, column = 1, sticky = W)
+            Label(self.RandomWindow, text = 'Valor Tabla: ').grid(row = 14, column = 0, sticky = W)
+            Label(self.RandomWindow, text = RandomChiCuadradoValorTabla).grid(row = 14, column = 1, sticky = W)
+            Label(self.RandomWindow, text = 'Suma de Frecuencias: ').grid(row = 15, column = 0, sticky = W)
+            Label(self.RandomWindow, text = RandomChiCuadradoSuma).grid(row = 15, column = 1, sticky = W)
+            Label(self.RandomWindow, text = '___________________________________________________', fg='black').grid(row = 16, column = 0)
+
+            ###############################################
+            #--  Generador Random Corrida Arriba Abajo -- #
+            ###############################################
+
+            Label(self.RandomWindow, text = 'Test Arriba Abajo de la Media.', font="Helvetica 11 bold").grid(row = 17, column = 0, sticky = W)
+            Label(self.RandomWindow, text = 'Resultado: ').grid(row = 18, column = 0, sticky = W)
+            if RandomChiCuadradoResult:
+                Label(self.RandomWindow, text = str(RandomChiCuadradoResult), fg = 'green').grid(row = 18, column = 1, sticky = W)
+            else:
+                Label(self.RandomWindow, text = str(RandomChiCuadradoResult), fg = 'red').grid(row = 18, column = 1, sticky = W)
+            Label(self.RandomWindow, text = 'Media: ').grid(row = 19, column = 0, sticky = W)
+            Label(self.RandomWindow, text = RandomChiCuadradoValorTabla).grid(row = 19, column = 1, sticky = W)
+            Label(self.RandomWindow, text = 'Z: ').grid(row = 20, column = 0, sticky = W)
+            Label(self.RandomWindow, text = RandomChiCuadradoSuma).grid(row = 20, column = 1, sticky = W)
+            Label(self.RandomWindow, text = '___________________________________________________', fg='black').grid(row = 21, column = 0)
+
 
             #NumPy Generator
             arrayNumPy = self.GeneradorNumPy(index, int(seed))
@@ -213,8 +235,8 @@ class Index:
         return resultado, D, d_estimada
 
     def ChiCuadrado(self, results):
-        valorTabla = 16.9190 # con alpha 0.05 y grado de libertad 9
-        freq_esperada = len(results)/10
+        valor_tabla = 16.9190 # con alpha 0.05 y grado de libertad 9
+        frec_estimada = len(results)/10
         frecuencias = np.zeros(10)
         intervalos = np.zeros(10)
         #divido la results en 10 intervalos
@@ -231,23 +253,58 @@ class Index:
             else: intervalos[9] = intervalos[9] + 1
 
         for i in range(0, len(frecuencias)):
-            frecuencias[i] = (intervalos[i]- freq_esperada)**2/freq_esperada
+            frecuencias[i] = (intervalos[i]- frec_estimada)**2/frec_estimada
 
         suma = sum(frecuencias)
 
-        print("Suma de frecuencias: "+str(suma)+ " , frecuencia esperada: "+str(freq_esperada)+" , valor en la tabla(alpha=0.05, grado libertad=9): 16.9190")
-        print("¿Suma de frecuencias < valor en tabla?")
-
-        if(suma<valorTabla):
+        if(suma<valor_tabla):
             resultado = True
         else:
             resultado = False
 
-        return resultado, 
+        return resultado, suma, valor_tabla, frec_estimada
 
     def CorridaArribaAbajo(self, results):
-        return True
+        listaOperadores = []
+        N = len(results)
+        n1 = 0
+        n2 = 0
+        b = 0
+        mediaMuestra = np.average(results)
+        mediaB = 0
+        varianzaB = 0
+        Z = 0
+        for l in results:
+            if l >= mediaMuestra:
+                listaOperadores.append('+')
+                n1 = n1 + 1 
+            else:
+                listaOperadores.append('-')
+                n2 = n2 + 1
+        for j in range(0,len(listaOperadores)-1):
+            if listaOperadores[j] == '+':
+                if listaOperadores[j] != listaOperadores[j+1]:
+                    b = b + 1
+            else:
+                if listaOperadores[j] != listaOperadores[j+1]:
+                    b = b + 1
+        mediaB = ((2*n1*n2)/(n1+n2))+1
+        varianzaB = (2*n1*n2*((2*n1*n2)-N))/(N*N*(N-1))
+        Z = (b - mediaB)/(np.sqrt(varianzaB))
+        #alfa = 0.05 , por lo tanto Z(1-(alfa/2)) = Z(0.025) = 1.96
 
+        print("Z calculada: "+str(Z)+" , Z(0.025) = 1.96")
+        print("¿|Z| < Z(0.025)?")
+
+        if abs(Z)<1.96:
+            print("Test aprobado. Se demuestra la Independecia, por lo tanto, la aleatoriedad también")
+            print("")
+            resultado = True
+        else:
+            print("Test desaprobado. Se rechaza la independencia")
+            print("")
+            resultado = False
+        return resultado, Z, mediaMuestra
 
         
 
